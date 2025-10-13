bf2360614553689bf0d6560e9ba4a0e03b9397c5
Emmanuel <22738@virtual.utsc.edu.mx>
2025-10-13 14:47:53 -0600

    Se añadieron los archivos selccionados con lasmodificacioes corresponientes y sus funcionalidades

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const themeToggler = document.getElementById('theme-toggler');
+    
+    const body = document.body;
+
+    const toggleTheme = () => {
+        body.classList.toggle('dark-mode');
+        
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+        }
+    };
+
+    themeToggler.addEventListener('click', toggleTheme);
+
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+        body.classList.add('dark-mode');
+    }
+
+    const envioInfo = document.getElementById("button");
+    const contacto = document.getElementById("contacto")
+    envioInfo.addEventListener("click", function(){
+        contacto.reset();
+        alert("Se ha enviado la información");
+        
+    })
+
+});
